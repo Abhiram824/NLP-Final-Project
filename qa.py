3fc19515045a4372f55d2dae7c3516dea119ebb4
Abhiram824 <66290411+Abhiram824@users.noreply.github.com>
2023-04-27 01:08:17 -0500

    Updates to qa.py to get short answers and their contexts

--- qa.py
+++ qa.py
@@ -1,83 +1,80 @@
 import json
-from bs4 import BeautifulSoup
-import csv
 import random
-
 import pandas as pd
 
-with open('data.jsonl', 'r') as f:
-    pairs = []
-    count = 0
-    for line in f:
-        if count >= 2000: #TODO CHANGE TO 10000
-            break
-        if count%10 == 0:
-            json_obj = json.loads(line)
+NUM_SAMPLES = 10000
+NUM_USING = 302
+PATH = "nq_data.jsonl"
+CONTEXT_WINDOW = 100
 
-            question_text = json_obj['question_text']
 
-            answer_start_token = json_obj['annotations'][0]['long_answer']['start_token']
-            answer_end_token = json_obj['annotations'][0]['long_answer']['end_token']
-            document_tokens = json_obj['document_tokens']
-            answer_tokens = document_tokens[answer_start_token:answer_end_token+1]
-            answer_text = ''
-            allowed = [',', '\'', '!', '.', '?', ':', ';', ')', ']', '}']
-            for i in range(len(answer_tokens)-1):
-                token = answer_tokens[i]['token']
-                next_token = answer_tokens[i+1]['token']
+def get_question(sample):
+    return sample['question_text']
 
-                if next_token in allowed:
-                    answer_text += token
-                else:
-                    if token in '([{':
-                        answer_text += token
-                    else:
-                        answer_text += token + ' '
+def tokenize_answer(document_text):
+    return document_text.split()
 
-            if len(answer_text) == 0:
-                continue
-            else:
-                answer_text = answer_text.rstrip()
-                answer_text += answer_tokens[-1]['token']
+def format_data(tokens):
+    return ' '.join(tokens)
 
-            soup = BeautifulSoup(answer_text, 'html.parser')
-            isBad = False
-            allowed_tags = {'p'}
-            for tag in soup.find_all():
-                if tag.name in allowed_tags:
-                    tag.extract()
-                if tag.name not in allowed_tags:
-                    isBad = True
 
-            if isBad:
-                continue
-            else:
-                answer_text = answer_text.replace('<P>', '')
-                answer_text = answer_text.replace('</P>', '')
+def get_short_answer_and_context(sample):
+    short_answers = sample['annotations'][0]['short_answers']
+    yes_no_answer = sample['annotations'][0]['yes_no_answer']
+    document_tokens = tokenize_answer(sample['document_text'])
+    #only want samples that are not yes/no questions and have short answers
+    if(len(short_answers) == 0 or yes_no_answer != "NONE"):
+        return None
+    
+    SA = []
+    contexts = []
+    for short_answer in short_answers:
+        start_index = short_answer['start_token']
+        end_index = short_answer['end_token']
+        context_start = max(0, start_index - CONTEXT_WINDOW)
+        context_end = min(len(document_tokens)-1, end_index + CONTEXT_WINDOW)
+        SA.append(format_data(document_tokens[start_index:end_index]))
+        contexts.append(format_data(document_tokens[context_start:context_end]))
+    return (SA, contexts)
 
-            # Print the question-answer pair
-            pairs.append((question_text, answer_text))
-        count += 1
 
-random.shuffle(pairs)
-pairs = pairs[:125]
+def read_data(path):
+    question_answer_pairs = []
+    counter = 0
+    with open("nq_data.jsonl", 'r') as f:
+        for line in f:
+            sample = json.loads(line)
 
-questions = [pair[0] for pair in pairs]
-answers = [pair[1] for pair in pairs]
-empty_arr = ["" for _ in range(len(pairs))]
+            info = get_short_answer_and_context(sample)
+            if info:
+                
+                question = get_question(sample)
+                question_answer_pairs.append((question, info[0], info[1]))
+                counter +=1
+            
+            if counter == NUM_SAMPLES:
+                break
+    return question_answer_pairs
 
-qa_dict = {"Questions": questions, "Gold Answers": answers, "GPT Answer": empty_arr, "BERT Similarity GPT": empty_arr,
+questions_answers = read_data(PATH)
+random.shuffle(questions_answers)
+questions_answers = questions_answers[:NUM_USING]
+questions = [triplet[0] for triplet in questions_answers]
+answers = [triplet[1] for triplet in questions_answers]
+contexts = [triplet[2] for triplet in questions_answers]
+empty_arr = ["" for i in range(NUM_USING)]
+
+qa_dict = {"Contexts": contexts, "Questions": questions, "Gold Answers": answers, "GPT Answer": empty_arr, "BERT Similarity GPT": empty_arr,
            "Base Model Answers": empty_arr, "BERT Similarity Baseline": empty_arr, "GPT Correctness":empty_arr, "Baseline Correctness": empty_arr}
 
 qa_df = pd.DataFrame(qa_dict)
 
 qa_df.to_csv("pairs.csv")
 
-#TODO add columns names
-# with open('pairs.csv', 'a', encoding='utf-8', newline='') as csvfile:
-#     # Create a writer object
-#     writer = csv.writer(csvfile)
-    
-#     # Write each tuple to the CSV file
-#     for row in pairs:
-#         writer.writerow(row)
+
+
+
+
+
+        
+       
